aca83e399fca05e4bca1fe506ece368272d5e96b
zhangxiaohai <1335221336@qq.com>
2022-09-28 18:25:57 +0800

    解决打包到服务器之后启动报错问题，升级springboot和springcloud版本。 暂时注释掉jetcache配置代码，有依赖循环没有解决，后面处理。

--- gradleMicro-common/gradleMicro-common-other/src/main/java/com/hai/micro/common/other/config/JetCacheRedisConfig.java
+++ gradleMicro-common/gradleMicro-common-other/src/main/java/com/hai/micro/common/other/config/JetCacheRedisConfig.java
@@ -1,21 +1,5 @@
 package com.hai.micro.common.other.config;
 
-import java.util.function.Function;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-
-import com.alicp.jetcache.CacheValueHolder;
-import com.alicp.jetcache.anno.support.SpringConfigProvider;
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
 /**
  * @ClassName JetCacheRedisConfig
  * @Description jetCache存储序列化配置
@@ -23,7 +7,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
  * @Date 2022/7/19 15:45
  * @Version 1.0
  **/
-@Configuration(proxyBeanMethods = false)
+/*@Configuration(proxyBeanMethods = false)
 public class JetCacheRedisConfig extends SpringConfigProvider {
 
     @Autowired
@@ -49,4 +33,4 @@ public class JetCacheRedisConfig extends SpringConfigProvider {
         objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
         return new GenericJackson2JsonRedisSerializer(objectMapper);
     }
-}
+}*/
